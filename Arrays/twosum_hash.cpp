698923de2189a02d87bbadf6c009a346af4c5c9f
Himanshu Kushwaha <kushwahahimanshu776@gmail.com>
2026-02-02 23:20:12 +0530

    Solved Two Sum using hashmap (unsorted array)

---
+++ Arrays/twosum_hash.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "Enter elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    int target;
+    cout << "Enter target: ";
+    cin >> target;
+
+    unordered_map<int, int> mp;  // value -> index
+
+    for (int i = 0; i < n; i++) {
+        int need = target - nums[i];
+
+        if (mp.count(need)) {
+            cout << "Indices: " << mp[need] << " " << i << endl;
+            return 0;
+        }
+
+        mp[nums[i]] = i;
+    }
+
+    cout << "No pair found" << endl;
+    return 0;
+}
